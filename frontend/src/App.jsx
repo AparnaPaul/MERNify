2dffde736a2f8062832a7170d5d3a074177a3f0a
Aparna Paul <aparnapaul55@gmail.com>
2025-03-30 22:49:25 +1030

    Front end - Login, Signup, View Product done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
-import { Button } from './components/ui/button'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/user/Home'
+import Login from './pages/shared/Login'
+import Signup from './pages/shared/Signup'
+import AdminDashboard from './pages/admin/AdminDashboard'
+import Navbar from "./components/Navbar"
+import Footer from './components/Footer'
+import "react-toastify/dist/ReactToastify.css";
+import { AuthProvider } from "./context/AuthContext";
+import Products from './pages/shared/Products'
+import Cart from './pages/user/Cart'
+import NotFound from './pages/user/NotFound'
+import ProductPage from './pages/shared/ProductPage'
 
 const App = () => {
+
+
   return (
-    <div>
-      <Button> Button</Button>
-    </div>
+    <>
+      <BrowserRouter>
+        <AuthProvider>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/signup' element={<Signup />} />
+            <Route path='/products' element={<Products />} />
+            <Route path='/product/:id' element={<ProductPage />} />
+            <Route path='/cart' element={<Cart />} />
+            <Route path='/*' element={<NotFound />} />
+            <Route path='/adminDashboard' element={<AdminDashboard />} />
+          </Routes>
+          <Footer />
+        </AuthProvider>
+      </BrowserRouter>
+    </>
   )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ThemeProvider } from './components/theme-provider'
+import { ProductProvider } from './context/ProductContext'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <ProductProvider>
+        <App />
+      </ProductProvider>
+    </ThemeProvider>
+
   </StrictMode>,
 )
